b8437c259e827042679c12d36da8649587394193
Nemanja Tozic <nemanjat94@gmail.com>
2022-07-26 15:36:45 +0200

    initial commit

---
+++ src/main/java/com/ntozic/airsoft/iam/AirsoftIamApplication.java
@@ -0,0 +1,13 @@
+package com.ntozic.airsoft.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirsoftIamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirsoftIamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ntozic/airsoft/iam/AirsoftIamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntozic.airsoft.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirsoftIamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
