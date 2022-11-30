694b0c7ac8d69126ea68431db1a4945262864159
Nemanja Tozic <nemanja.tozic@quickbit.com>
2022-11-30 13:50:03 +0100

    switch to jakarta

--- src/main/java/com/ntozic/airsoft/iam/config/grpc/AirsoftGrpcAuthenticationReader.java
+++ src/main/java/com/ntozic/airsoft/iam/config/grpc/AirsoftGrpcAuthenticationReader.java
@@ -2,11 +2,10 @@ package com.ntozic.airsoft.iam.config.grpc;
 
 import io.grpc.Metadata;
 import io.grpc.ServerCall;
+import jakarta.annotation.Nullable;
 import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-
-import javax.annotation.Nullable;
 
 public class AirsoftGrpcAuthenticationReader implements GrpcAuthenticationReader {
     @Nullable
